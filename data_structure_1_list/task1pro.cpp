a053407dec08ca25042249b303aa4aa27e792ff6
hus-king <3334991525@qq.com>
2025-04-15 19:17:48 +0800

    修复顺序表结构体中元素指针的初始化，确保内存安全

--- data_structure_1_list/task1pro.cpp
+++ data_structure_1_list/task1pro.cpp
@@ -20,7 +20,7 @@ typedef int ElemType; // 数据元素类型定义
 
 // 顺序表（顺序结构）的定义
 typedef struct {
-    ElemType* elem; // 数据元素存储空间的基址
+    ElemType* elem = NULL; // 数据元素存储空间的基址
     int length;     // 当前长度
     int listsize;   // 当前分配的存储容量
 } SqList;
